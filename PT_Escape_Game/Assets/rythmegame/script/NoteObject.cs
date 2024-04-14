1a3a3d13ee5aa3f0ddfe16f0715cdc2350907d14
Akaniii <95611827+Akaniii@users.noreply.github.com>
2024-04-14 19:16:36 +0200

    bijour

---
+++ PT_Escape_Game/Assets/rythmegame/script/NoteObject.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteObject : MonoBehaviour
+{
+    public bool CanBepressed = false;
+    public KeyCode keyToPress;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyToPress))
+        {
+            if (CanBepressed)
+            {
+                gameObject.SetActive(false);
+                GameManager1.instance.NoteHit();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag=="Activator")
+        {
+            CanBepressed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Activator"&&gameObject.activeSelf)
+        {
+            CanBepressed = false;
+            GameManager1.instance.NoteMissed();
+        }
+    }
+}
